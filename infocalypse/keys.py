d6949bb62e45fb53f52d9f93afaa404e4347d847
Steve Dougherty <steve@asksteved.com>
2013-07-18 16:25:39 -0400

    Remove 'freenet://' from a USK more explicitly.

--- infocalypse/keys.py
+++ infocalypse/keys.py
@@ -14,8 +14,8 @@ class USK:
         # TODO: Is stripping "freenet://" appropriate?
         if self.key.startswith('freenet:'):
             self.key = self.key[len('freenet:'):]
-            if self.key.startswith('//'):
-                self.key = self.key[len('//'):]
+        elif self.key.startswith('freenet://'):
+            self.key = self.key[len('freenet://'):]
 
     def __str__(self):
         return '%s/%s/%s' % (self.key, self.name, self.edition)
